873e15169d75f50d8e05bad02f728cc591418645
Lucas Santos <lucas.santos-sc@airliquide.com@MacBook-Pro-de-Lucas-2.local>
2025-10-23 13:24:49 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+import {HomeScreen} from './presentation/screens/Home.tsx'
+
+export const App: React.FC = () => <HomeScreen />
---
+++ src/presentation/screens/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HomeScreen: React.FC = () => {
+    return (
+        <h1>Home</h1>
+    )
+}
